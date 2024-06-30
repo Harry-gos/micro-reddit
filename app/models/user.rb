14063428f104f8045b1d4e4fdbfffc8c2a72d18e
Hung Pham <harry.pham.go@gmail.com>
2024-06-30 23:08:58 +0700

    Add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy
-
+    has_many :comments, dependent: :destroy
     validates :username, presence: true, length: {minimum: 4, maximum: 12},  uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 16}
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_30_144727) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_30_151204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "link"
@@ -32,5 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_30_144727) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
