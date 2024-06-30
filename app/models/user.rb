8fe36178d05631257af718feb2bc5d1e4a06e916
Hung Pham <harry.pham.go@gmail.com>
2024-06-30 22:08:06 +0700

    Add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts, dependent: :destroy
+
     validates :username, presence: true, length: {minimum: 4, maximum: 12},  uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 16}
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_30_093619) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_30_144727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "link"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -24,4 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_30_093619) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
