16bb6bc3e85b526ff31fa38d2efc062d74ea9462
Hung Pham <harry.pham.go@gmail.com>
2024-06-30 23:46:00 +0700

    Add test fixtures and 1 test for Comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "relations between comments, posts, and users" do
+    u2 = User.find(2)
+    assert u2 == users(:user2), "should get 'user2'"
+
+    c1 = u2.comments.first
+    assert c1 == comments(:c1), "should return that user's comment"
+
+    p1 = Post.first
+    assert p1.comments.first == c1, "should return the comment 'c1'"
+
+    assert c1.post == p1, "should return the post 'p1'"
+  end
 end
