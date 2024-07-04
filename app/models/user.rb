b277b0db519daab0f23b492c62eaf01aa0402906
Hung Pham <harry.pham.go@gmail.com>
2024-07-04 18:06:59 +0700

    Add password_disgest

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     validates :username, presence: true, length: {minimum: 4, maximum: 12},  uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 16}
+
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_30_151204) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_04_100258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,9 +35,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_30_151204) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
